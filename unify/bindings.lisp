717b38d7a6e3cde80fe05ceaed41ec71428e6dd3
Moskvitin Andrey <archimag@gmail.com>
2010-04-09 18:22:57 +0400

    refactoring

--- unify/bindings.lisp
+++ unify/bindings.lisp
@@ -13,29 +13,21 @@
 (defvar +no-bindings+ '((nil))
   "Indicates unification success, with no variables.")
 
-
 (defun get-binding (var bindings)
   "Find a (variable . value) pair in a binding list."
   (assoc var bindings))
 
-(defun binding-val (binding)
-  "Get the value part of a single binding."
-  (cdr binding))
-
-(defun make-binding (var val) (cons var val))
-
 (defun lookup (var bindings)
   "Get the value part (for var) from a binding list."
-  (binding-val (get-binding var bindings)))
+  (cdr (get-binding var bindings)))
 
 (defun extend-bindings (var val bindings)
   "Add a (var . value) pair to a binding list."
-  (cons (make-binding var val)
-        ;; Once we add a "real" binding,
-        ;; we can get rid of the dummy +no-bindings+
-        (if (eq bindings +no-bindings+)
-            nil
-            bindings)))
+  (acons var
+         val
+         (if (eq bindings +no-bindings+)
+             nil
+             bindings)))
 
 (defun occurs-in-p (var x bindings)
   "Does var occur anywhere inside x?"
